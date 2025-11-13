47108cf36ad786a952e3ef033907819cd20fcf92
Juh5216 <161980749+Juh5216@users.noreply.github.com>
2025-11-13 22:13:27 +0000

    - Inclusão de código

--- EntradaSaidaDados/SomaNumeros.kt
+++ EntradaSaidaDados/SomaNumeros.kt
@@ -1,1 +1,16 @@
-.kt
+import java.util.Scanner;
+
+fun main() {
+
+    print("Insira o primeiro número: ")
+    val scanner = Scanner(System.`in`);
+    val num1 = scanner.nextInt();
+
+    print("Insira o segundo número: ")
+    val num2 = scanner.nextInt();
+
+    val resultado = num1 + num2;
+
+    println("A soma dos dois números é: $resultado ");
+
+}
