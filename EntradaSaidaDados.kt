a0cec65c8b0179870c160814b1c2f2460d649104
Juh5216 <161980749+Juh5216@users.noreply.github.com>
2025-11-13 21:43:57 +0000

    - exclusão de arquivos

--- EntradaSaidaDados.kt
+++ EntradaSaidaDados.kt
@@ -3,7 +3,7 @@ import java.util.Scanner;
 fun main() {
 
     //Nome
-    print("Digite o se nome: ")
+    print("Digite o seu nome: ")
     var scanner = Scanner(System.`in`);
     var nome = scanner.next();
 
