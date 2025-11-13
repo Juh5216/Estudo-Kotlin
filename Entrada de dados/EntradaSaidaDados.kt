a8993787c3e554db96aafd0720a9405dba7a8df1
Juh5216 <161980749+Juh5216@users.noreply.github.com>
2025-11-13 18:49:01 +0000

    - estruturação do código

--- Entrada de dados/EntradaSaidaDados.kt
+++ Entrada de dados/EntradaSaidaDados.kt
@@ -1,1 +1,23 @@
+// Escreva um programa que solicite ao usuário a entrada de dois números inteiros. O programa deve ler esses dois números, calcular a soma deles e, em seguida, exibir o resultado da soma na tela com uma mensagem descritiva (ex: "A soma é: X").
 
+import java.util.Scanner;
+
+fun main() {
+
+    //Declaração das variáveis
+    var num1;
+    var num2;
+ 
+    // Solicitação e recebimento de dados do usuário
+    print("Digite o primeiro número: ")
+    var scanner = Scanner(System.`in`);
+    num1 = scanner.nextInt();
+
+    print("Digite o segundo número: ")
+    var scanner = Scanner(System.`in`);
+    num2 = scanner.nextInt();
+
+
+
+
+}
