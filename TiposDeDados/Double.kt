e35e83731f5126a05e1965feab0f27b851464ce4
Juh5216 <161980749+Juh5216@users.noreply.github.com>
2025-11-18 00:47:44 +0000

    - Criação de novo arquivo

---
+++ TiposDeDados/Double.kt
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+fun main() {
+    var num:Double;
+    var resultado:Double;
+    // Recebimento do número
+    print("Insira um número: ")
+    val scanner=Scanner(System.`in`)
+    num = scanner.nextDouble();
+    // Cálculo do número
+    resultado=num/2
+    // Exibição do número
+    println("A metade do número $num é $resultado")
+}
